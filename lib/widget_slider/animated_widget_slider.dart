25ec8fed3b9718eddb22ea319dc99e47b3899be2
andreloussin <loussin.andre@gmail.com>
2022-06-15 19:38:56 +0100

    Update animated_widget_slider.dart

--- lib/widget_slider/animated_widget_slider.dart
+++ lib/widget_slider/animated_widget_slider.dart
@@ -6,10 +6,26 @@ import 'package:flutter_animated_widget_slide/main.dart';
 class AnimatedWidgetSlider extends StatefulWidget {
   final double width;
   late final Widget? initial;
-  late final List<Widget>? contents;
-  AnimatedWidgetSlider(
-      {Key? key, required this.width, this.initial, this.contents})
-      : super(key: key);
+  late List<Widget>? contents = [
+    const MyHomePage(title: "1"),
+    const MyHomePage(title: "2"),
+    const MyHomePage(title: "3"),
+    const MyHomePage(title: "4"),
+    const MyHomePage(title: "5"),
+  ];
+  AnimatedWidgetSlider({Key? key, required this.width, this.initial, contents})
+      : super(key: key) {
+    this.contents = contents != null
+        ? contents
+        : [
+            const MyHomePage(title: "1"),
+            const MyHomePage(title: "2"),
+            const MyHomePage(title: "3"),
+            const MyHomePage(title: "4"),
+            const MyHomePage(title: "5"),
+          ];
+    ;
+  }
   _AnimatedWidgetSliderState _bi = _AnimatedWidgetSliderState();
 
   @override
@@ -27,11 +43,11 @@ class AnimatedWidgetSlider extends StatefulWidget {
 class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
     with TickerProviderStateMixin {
   late AnimationController controller;
-  late Animation anim1;
-  late Animation anim2;
-  late Animation anim3;
-  late Animation anim4;
-  final GlobalKey _widgetKey = GlobalKey();
+  late Animation rotateAnim;
+  late Animation translateAnim;
+  late Animation firstScaleAnim;
+  late Animation lastScaleAnim;
+  final GlobalKey widgetKey = GlobalKey();
   late RenderBox? renderBox;
   bool _turnNormal = true;
   Widget? _actual;
@@ -46,27 +62,27 @@ class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
     //_wid2 = MyHomePage(title: '222222222222222222222');
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 500),
+      duration: const Duration(seconds: 2),
     );
-    anim1 = Tween<double>(begin: 0, end: math.pi / 2).animate(
+    rotateAnim = Tween<double>(begin: 0, end: math.pi / 2).animate(
       CurvedAnimation(
         parent: controller,
         curve: const Interval(0.0, 1.0),
       ),
     );
-    anim2 = Tween<double>(begin: 0, end: widget.width).animate(
+    translateAnim = Tween<double>(begin: 0, end: widget.width).animate(
       CurvedAnimation(
         parent: controller,
         curve: const Interval(0.0, 1.0),
       ),
     );
-    anim3 = Tween<double>(begin: 1, end: 0.9).animate(
+    firstScaleAnim = Tween<double>(begin: 1, end: 0.9).animate(
       CurvedAnimation(
         parent: controller,
         curve: const Interval(0.0, 0.5),
       ),
     );
-    anim4 = Tween<double>(begin: 0.9, end: 1).animate(
+    lastScaleAnim = Tween<double>(begin: 0.9, end: 1).animate(
       CurvedAnimation(
         parent: controller,
         curve: const Interval(0.5, 1.0),
@@ -78,35 +94,44 @@ class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onPanUpdate: (details) {
-        if (details.delta.dx > 0)
-          fromLeft(widget: MyHomePage(title: "LEFT"));
+      onHorizontalDragEnd: (details) {
+        if (details.velocity.pixelsPerSecond.dx > 0)
+          fromLeft();
         else
-          fromRight(widget: MyHomePage(title: "RIGHT"));
+          fromRight();
       },
       child: Stack(
-        key: _widgetKey,
+        key: widgetKey,
         alignment: Alignment.center,
         children: [
           AnimatedBuilder(
             animation: controller,
             builder: (BuildContext _, child) {
-              // Offset _offset = Offset(0, anim1.value);
+              // Offset _offset = Offset(0, rotateAnim.value);
               return Transform(
                 alignment: Alignment.center,
                 transform: _turnNormal
                     ? (Matrix4.identity()
                       ..setEntry(3, 2, 0.001)
-                      ..rotateY(anim1.value)
-                      ..scale(anim3.value)
-                      ..scale(anim4.value)
-                      ..translate(-anim2.value, 0, -anim2.value))
+                      ..rotateY(rotateAnim.value)
+                      // ..scale(rotateAnim.value < math.pi / 4
+                      //     ? firstScaleAnim.value
+                      //     : lastScaleAnim.value)
+                      ..translate(
+                          -(widget.width * math.sin(rotateAnim.value)) / 2,
+                          0,
+                          -(widget.width * (1 - math.cos(rotateAnim.value))) /
+                              2))
+                    // ..translate(
+                    //     -translateAnim.value, 0, -translateAnim.value))
                     : (Matrix4.identity()
                       ..setEntry(3, 2, 0.001)
-                      ..rotateY(-anim1.value)
-                      ..scale(anim3.value)
-                      ..scale(anim4.value)
-                      ..translate(anim2.value, 0, -anim2.value)),
+                      ..rotateY(-rotateAnim.value)
+                      ..scale(rotateAnim.value < math.pi / 4
+                          ? firstScaleAnim.value
+                          : lastScaleAnim.value)
+                      ..translate(
+                          translateAnim.value, 0, -translateAnim.value)),
                 child: child,
               );
             },
@@ -120,20 +145,22 @@ class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
                 transform: _turnNormal
                     ? (Matrix4.identity()
                       ..setEntry(3, 2, 0.001)
-                      ..scale(anim3.value)
-                      ..scale(anim4.value)
-                      ..rotateY(anim1.value - math.pi / 2)
-                      ..translate(widget.width - anim2.value, 0,
-                          -widget.width + anim2.value))
+                      // ..scale(rotateAnim.value < math.pi / 4
+                      //     ? firstScaleAnim.value
+                      //     : lastScaleAnim.value)
+                      ..rotateY(rotateAnim.value - math.pi / 2)
+                      ..translate(widget.width - translateAnim.value, 0,
+                          -widget.width + translateAnim.value))
                     : (Matrix4.identity()
                       ..setEntry(3, 2, 0.001)
-                      ..scale(anim3.value)
-                      ..scale(anim4.value)
-                      ..rotateY(math.pi / 2 - anim1.value)
+                      ..scale(rotateAnim.value < math.pi / 4
+                          ? firstScaleAnim.value
+                          : lastScaleAnim.value)
+                      ..rotateY(math.pi / 2 - rotateAnim.value)
                       ..translate(
-                        anim2.value - widget.width,
+                        translateAnim.value - widget.width,
                         0,
-                        anim2.value - widget.width,
+                        translateAnim.value - widget.width,
                       )),
                 child: child,
               );
@@ -146,9 +173,21 @@ class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
   }
 
   void fromRight({Widget? widget}) {
-    widget ??= Container();
     setState(() {
+      if (widget == null) {
+        try {
+          widget = this
+              .widget
+              .contents!
+              .elementAt(index % this.widget.contents!.length);
+          index++;
+        } catch (e) {
+          widget = Container();
+        }
+      }
       _turnNormal = true;
+      print(_turnNormal);
+      print(index);
       if (_wid2 == _actual) _wid1 = _wid2;
       _wid2 = widget;
       _actual = _wid2;
@@ -158,8 +197,18 @@ class _AnimatedWidgetSliderState extends State<AnimatedWidgetSlider>
   }
 
   void fromLeft({Widget? widget}) {
-    widget ??= Container();
     setState(() {
+      if (widget == null) {
+        try {
+          widget = this
+              .widget
+              .contents!
+              .elementAt(index % this.widget.contents!.length);
+          index--;
+        } catch (e) {
+          widget = Container();
+        }
+      }
       _turnNormal = false;
       if (_wid2 == _actual) _wid2 = _wid1;
       _wid2 = widget;
